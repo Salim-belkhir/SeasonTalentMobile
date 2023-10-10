3a0f4e83e8e4bfb345fbb9bf8dedab9a820d8a51
ayoubbibo <hakemiayoub612@gmail.com>
2023-10-10 17:47:30 +0200

    Comments update

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,12 @@
 import { createStore } from "redux";
 import myReducer from "./reducers";
 
-// Create a store
+/**
+ * Creates a Redux store with the specified reducer function.
+ * @function
+ * @param {Function} reducer - The reducer function for the store.
+ * @returns {Object} - The created Redux store.
+ */
 const store = createStore(myReducer);
 
 export default store;
