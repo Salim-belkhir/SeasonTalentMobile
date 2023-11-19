20e71da7171cd3195c597bd3abc41bc43507476d
ayoubbibo <Hakemiayoub612@gmail.com>
2023-11-19 01:30:00 +0100

    almost done the creation of company"

--- src/redux/reducers/companyReducer.js
+++ src/redux/reducers/companyReducer.js
@@ -7,24 +7,28 @@ const initialState = {
       name: "Google",
       address: "1600 Amphitheatre Pkwy, Mountain View, CA 94043",
       logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1024px-Google_%22G%22_Logo.svg.png",
+      contact: "google@gmail.com",
     },
     {
       id: 4,
       name: "Microsoft",
       address: "One Microsoft Way, Redmond, WA 98052",
       logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Microsoft_logo.svg/1024px-Microsoft_logo.svg.png",
+      contact: "microsoft@outlook.com",
     },
     {
       id: 5,
       name: "Netflix",
       address: "100 Winchester Cir, Los Gatos, CA 95032",
       logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1024px-Netflix_2015_logo.svg.png",
+      contact: "netftix@netflix.com",
     },
     {
       id: 6,
       name: "Tesla",
       address: "45500 Fremont Blvd, Fremont, CA 94538",
       logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Tesla_Motors.svg/1024px-Tesla_Motors.svg.png",
+      contact: "tesla@swift.com",
     },
   ],
   principalCompany: {
@@ -32,6 +36,7 @@ const initialState = {
     name: "Airbnb",
     address: "888 Brannan St, San Francisco, CA 94103",
     logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/1200px-Airbnb_Logo_B%C3%A9lo.svg.png",
+    constact: "airbnb@gmail.com",
   },
   selectedCompany: null,
   loading: false,
@@ -58,7 +63,16 @@ const companyReducer = (state = initialState, action) => {
     case companiesActions.CREATE_COMPANY:
       return {
         ...state,
-        companies: [...state.companies, action.payload],
+        companies: [
+          ...state.companies,
+          {
+            id: state.companies.length + 1,
+            name: action.payload.title,
+            address: action.payload.address,
+            logo: action.payload.logo,
+            ...action.payload,
+          },
+        ],
         loading: true,
         error: null,
       };
